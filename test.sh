ba83035d3a605223fba1ae66147ba3f23ad629ca
Michael V Thanh <mthanh@circleci.com>
2019-12-20 12:31:33 -0700

    Another test

--- test.sh
+++ test.sh
@@ -5,3 +5,5 @@ echo $TEST_VAR
 echo "TEST_VAR=$TEST_VAR"
 echo "test_var=$TEST_VAR"
 echo "test_var=${TEST_VAR}"
+echo "test_var--->${TEST_VAR}"
+echo "test_var--->${ANOTHER_TEST_VAR}@example.io/1234567"
